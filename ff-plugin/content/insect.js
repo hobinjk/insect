667c4747b93161bfdf8f8f827e517b2c134fbab8
James Hobin <hobinjk@mit.edu>
2013-08-23 17:28:28 -0400

    Improve exception logging in plugin

--- ff-plugin/content/insect.js
+++ ff-plugin/content/insect.js
@@ -3,14 +3,14 @@ var Insect = {
   start: function() {
     console.log("insect");
     Cu.import('resource://gre/modules/devtools/dbg-server.jsm');
-    if (!DebuggerServer.initialized) {
+    if (!DebuggerServer.initialized || true) {
       DebuggerServer.init(() => true);
       DebuggerServer.addBrowserActors();
 
       try {
         DebuggerServer.openListener(6000);
       } catch (e) {
-        dump('Unable to start debugger server: ' + e + '\n');
+        console.log('Unable to start debugger server: ' + e + '\n');
       }
       console.dir(DebuggerServer);
     }
